795bdf39548913bb273078a4af64b51d254ad4fb
WilsonLi <ligokdoon@gmail.com>
2021-02-16 19:10:45 -0500

    2-16

--- 2.16.js
+++ 2.16.js
@@ -51,10 +51,43 @@ var maxArea = function (height) {
     return max;
 }
 
-// Given a string containing digits from 2 - 9 inclusive, return all possible letter combinations that the number could represent.Return the answer in any order.
 
-// A mapping of digit to letters(just like on the telephone buttons) is given below.Note that 1 does not map to any letters.
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
 // Example 1:
-// Input: digits = "23"
-// Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+// Input: head = [1, 2, 3, 4, 5], n = 2
+// Output: [1, 2, 3, 5]
+
+// Example 2:
+// Input: head = [1], n = 1
+// Output: []
+
+// Example 3:
+// Input: head = [1, 2], n = 1
+// Output: [1]
+
+var removeNthFromEnd = function (head, n) {
+    const visited = [head];
+
+    while (visited[visited.length - 1].next) {
+        let currNode = visited[visited.length - 1];
+        visited.push(currNode.next);
+    }
+
+    visited.splice(-n, 1);
+
+    if (!visited.length) return null;
+
+    console.log(visited);
+
+    for (let i = 0; i < visited.length; i++) {
+        let currNode = visited[i];
+        if (i === visited.length - 1) {
+            currNode.next = null;
+        } else {
+            currNode.next = visited[i + 1];
+        }
+    }
+
+    return visited[0];
+};
