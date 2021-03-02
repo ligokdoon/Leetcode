f5e7a56e8123d4925253ce90293dbbb96e1b3975
WilsonLi <ligokdoon@gmail.com>
2021-03-01 19:42:34 -0500

    3-1

--- 3-1.js
+++ 3-1.js
@@ -61,4 +61,58 @@ var reverseWords = function (s) {
     return result.reverse().join(' ');
 };
 
-console.log(reverseWords('the sky is blue'))
+
+// A peak element is an element that is strictly greater than its neighbors.
+
+// Given an integer array nums, find a peak element, and return its index.If the array contains multiple peaks, return the index to any of the peaks.
+
+// You may imagine that nums[-1] = nums[n] = -∞.
+
+// Example 1:
+// Input: nums = [1, 2, 3, 1]
+// Output: 2
+// Explanation: 3 is a peak element and your function should return the index number 2.
+
+// Example 2:
+// Input: nums = [1, 2, 1, 3, 5, 6, 4]
+// Output: 5
+// Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+
+// var findPeakElement = function (nums) {
+//     if (nums.length < 2) return 0;
+
+//     for (let i = 0; i < nums.length; i++) {
+//         let num = nums[i];
+//         if (num > nums[i - 1] && num > nums[i + 1]) {
+//             return i;
+//         }
+//     }
+
+//     for (let i = 0; i < nums.length; i++) {
+//         let num = nums[i];
+
+//         if (!nums[i - 1] && num > nums[i + 1]) return i;
+//         if (!nums[i + 1] && num > nums[i - 1]) return i;
+//     }
+// };
+
+var findPeakElement = function (nums) {
+    if (nums.length < 2) return 0;
+
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2)
+
+        if (nums[mid] > nums[mid + 1]) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+
+    }
+    return left;
+};
+
+console.log(findPeakElement([1,3,5,3,4,1,2]))
